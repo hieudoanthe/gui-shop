a1dec5449bbe7fdd48e2667aefe7df442da18411
The Hieu <thehieu0814@gmail.com>
2023-11-02 11:59:13 +0700

    pttk

--- eshopper/assets/js/config.js
+++ eshopper/assets/js/config.js
@@ -1,9 +1,3 @@
-/**
- * Config
- * -------------------------------------------------------------------------------------
- * ! IMPORTANT: Make sure you clear the browser local storage In order to see the config changes in the template.
- * ! To clear local storage: (https://www.leadshook.com/help/how-to-clear-local-storage-in-google-chrome-browser/).
- */
 
 'use strict';
 
